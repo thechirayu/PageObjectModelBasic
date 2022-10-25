18a8dd09e0ad5e8bd12541e6c802f810ae8216be
thechirayu <chirayu.maheshwari@gmail.com>
2022-10-25 13:55:23 +0530

    New changes commit in src

--- src/test/java/com/chirayu/crm/accounts/AccountsPage.java
+++ src/test/java/com/chirayu/crm/accounts/AccountsPage.java
@@ -6,18 +6,17 @@ import com.chirayu.base.Page;
 
 public class AccountsPage extends Page {
 	
-	public AccountsPage() throws Exception {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+	
 
-	public void createAccount()
+	public void createAccount() throws InterruptedException
 	{
 		driver.findElement(By.xpath(OR.getProperty("CreateAccounButtom"))).click();
 		
 		driver.findElement(By.id(OR.getProperty("AccountName_Field"))).sendKeys("Demo");
 		
-		driver.findElement(By.id(OR.getProperty("PhoneNumber_Field"))).sendKeys("+919785420412");;
+		driver.findElement(By.id(OR.getProperty("PhoneNumber_Field"))).sendKeys("+919785420412");
+		
+		Thread.sleep(5000);
 	}
 	
 	public void importAccount()
